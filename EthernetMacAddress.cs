f2ad035eb716d312ae5acd6cfd6d51a6164ee67d
Nicolai Hoffmann <nicolai.hoffmann@t-online.de>
2016-03-04 15:21:13 +0100

    Added files via upload

---
+++ EthernetMacAddress.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace WoLib
+{
+    public class EthernetMacAddress
+    {
+        public byte[] mac_address
+        {
+            get; private set;
+        }
+
+        public EthernetMacAddress(byte a, byte b, byte c, byte d, byte e, byte f)
+        {
+            mac_address = new byte[6];
+            mac_address[0] = a;
+            mac_address[1] = b;
+            mac_address[2] = c;
+            mac_address[3] = d;
+            mac_address[4] = e;
+            mac_address[5] = f;            
+        }
+
+        public EthernetMacAddress(byte[] hex_address)
+        {
+            if (hex_address.Length > 6)
+            {
+                throw new Exception("Hex address too long! Has to be six bytes!");
+            }else if (hex_address.Length < 6)
+            {
+                throw new Exception("Hex address too short! Has to be six bytes!");
+            }
+
+            mac_address = hex_address;
+        }
+
+        public override string ToString()
+        {
+            string info = "";
+
+            foreach (byte b in mac_address)
+            {
+                info += b.ToString("x") + ":";
+            }
+
+            info = info.Remove(info.Length-1);
+
+            return info;
+        }
+    }
+}
---
+++ WoLWake.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace WoLib
+{
+    public class WoLWake
+    {
+        public EthernetMacAddress device_mac_address { get; private set; }
+        public byte[] magic_packet {get; private set;}     
+        public string ip { get; set; }  
+        public string broadcast_address { get; private set; }
+
+        //Constructor with EthernetMacAddress object
+        public WoLWake(string ip, EthernetMacAddress device_mac_address)
+        {
+            this.device_mac_address = device_mac_address;
+            this.ip = ip;
+            broadcast_address = IpToBraodcastAddress(this.ip);
+
+            magic_packet = new byte[102];
+
+            //Fill in six times 0xFF
+            for (int i = 0; i < 6; i++)
+            {
+                magic_packet[i] = 0xff;
+            }
+
+            //Fill in 16x ethernet mac address
+            for (int i = 0; i < 16; i++)
+            {
+                magic_packet[i * 6 + 5] = device_mac_address.mac_address[0];
+                magic_packet[i * 6 + 6] = device_mac_address.mac_address[1];
+                magic_packet[i * 6 + 7] = device_mac_address.mac_address[2];
+                magic_packet[i * 6 + 8] = device_mac_address.mac_address[3];
+                magic_packet[i * 6 + 9] = device_mac_address.mac_address[4];
+                magic_packet[i * 6 + 10] = device_mac_address.mac_address[5];
+            }            
+        }
+
+        //Constructor with mac address as byte array
+        public WoLWake(string ip, byte[] hex_address)
+        {
+            if (hex_address.Length > 6)
+            {
+                throw new Exception("Hex address too long! Has to be six bytes!");
+            }
+            else if (hex_address.Length < 6)
+            {
+                throw new Exception("Hex address too short! Has to be six bytes!");
+            }
+
+            device_mac_address = new EthernetMacAddress(hex_address);
+            this.ip = ip;
+            broadcast_address = IpToBraodcastAddress(this.ip);
+
+            magic_packet = new byte[102];
+
+            //Fill in six times 0xFF
+            for (int i = 0; i < 6; i++)
+            {
+                magic_packet[i] = 0xff;
+            }
+
+            //Fill in 16x ethernet mac address
+            for (int i = 0; i < 16; i++)
+            {
+                magic_packet[i * 6 + 5] = hex_address[0];
+                magic_packet[i * 6 + 6] = hex_address[1];
+                magic_packet[i * 6 + 7] = hex_address[2];
+                magic_packet[i * 6 + 8] = hex_address[3];
+                magic_packet[i * 6 + 9] = hex_address[4];
+                magic_packet[i * 6 + 10] = hex_address[5];
+            }
+        }
+
+        //Constructor with mac address as single bytes
+        public WoLWake(string ip, byte a, byte b, byte c, byte d, byte e, byte f)
+        {
+            device_mac_address = new EthernetMacAddress(a,b,c,d,e,f);
+            this.ip = ip;
+            broadcast_address = IpToBraodcastAddress(this.ip);
+
+            magic_packet = new byte[102];
+
+            //Fill in six times 0xFF
+            for (int i = 0; i < 6; i++)
+            {
+                magic_packet[i] = 0xff;
+            }
+
+            //Fill in 16x ethernet mac address
+            for (int i = 0; i < 16; i++)
+            {
+                magic_packet[i * 6 + 5] = a;
+                magic_packet[i * 6 + 6] = b;
+                magic_packet[i * 6 + 7] = c;
+                magic_packet[i * 6 + 8] = d;
+                magic_packet[i * 6 + 9] = e;
+                magic_packet[i * 6 + 10] = f;
+            }
+        }
+
+        public void Wake()
+        {
+            UdpClient s = new UdpClient(broadcast_address, 49153);
+            s.Send(magic_packet, magic_packet.Length);
+            s.Close();
+        }
+
+        private string IpToBraodcastAddress(string ip)
+        {
+            string broadcast_address = "";
+            int last_dot = ip.LastIndexOf(".");
+            broadcast_address = ip.Remove(last_dot + 1) + "255";
+            return broadcast_address;
+        }
+    }
+}
